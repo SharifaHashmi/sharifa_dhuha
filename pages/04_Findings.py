4043c954075a54f20c70d34aaa885b90f1e839b7
SharifaHashmi <121220012+SharifaHashmi@users.noreply.github.com>
2022-12-22 20:40:14 +0400

    Add files via upload

---
+++ pages/04_Findings.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.header("Findings")
+st.write("As noticed in the upcoming pages, with a total of 250 movies, we were able to pair the rating with the year and the title, the highest rate being 9.3 with the movie ‘The Shawshank Redemption’ (1994). The lowest rating being 8.0 with ‘Slumdog Millionaire’ (2008). Regarding Years, the oldest movie on the list was ‘The Kid’ which was released in 1921, and the most recent was ‘Guardians of the Galaxy Vol. 2’ which was released in 2017")
